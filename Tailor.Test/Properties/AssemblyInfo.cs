0ad34fac774690a6a4e1d067dee2cad9e0cca7ed
Andrew Best <andrew.best@readify.net>
2017-06-29 11:35:02 +1000

    Fixes project refs and updates AssemblyInfo

--- Tailor.Test/Properties/AssemblyInfo.cs
+++ Tailor.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tailor.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Tailor.Test")]
+[assembly: AssemblyDescription("A set of opinionated Query abstractions and accompanying Convention Tests to make sure your Dapper queries measure up")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tailor.Tests")]
+[assembly: AssemblyCompany("Andrew Best")]
+[assembly: AssemblyProduct("Tailor.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
--- Tailor/Properties/AssemblyInfo.cs
+++ Tailor/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Tailor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A set of opinionated Query abstractions and accompanying Convention Tests to make sure your Dapper queries measure up")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Andrew Best")]
 [assembly: AssemblyProduct("Tailor")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
