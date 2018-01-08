62e7336450c99545409b2c8d9f5728c3dfab9852
Leigh Metzroth <leigh.metzroth@businessfitness.net>
2018-01-08 12:35:34 +1000

    Changed it to accept a connection factory as a Func to build the connection factories.

--- Tailor.Test/TheTailor.cs
+++ Tailor.Test/TheTailor.cs
@@ -8,19 +8,19 @@ namespace Tailor.Test
 {
     public class TheTailor
     {
-        private readonly IConnectionFactory _connectionFactory;
+        private readonly Func<IConnectionFactory> _connectionFactoryBuilder;
         private readonly IEnumerable<Type> _dapperQueries;
         private readonly IEnumerable<Type> _dapperQueriesThatHaveNoParameters;
         private readonly IEnumerable<Type> _dapperQueryParameters;
 
         protected TheTailor(string connectionString, IEnumerable<Type> dapperQueries, IEnumerable<Type> dapperQueriesThatHaveNoParameters, IEnumerable<Type> dapperQueryParameters)
-            : this(new ConnectionFactory(connectionString), dapperQueries, dapperQueriesThatHaveNoParameters, dapperQueryParameters)
+            : this(() => new ConnectionFactory(connectionString), dapperQueries, dapperQueriesThatHaveNoParameters, dapperQueryParameters)
         {
         }
 
-        protected TheTailor(IConnectionFactory connectionFactory, IEnumerable<Type> dapperQueries, IEnumerable<Type> dapperQueriesThatHaveNoParameters, IEnumerable<Type> dapperQueryParameters)
+        protected TheTailor(Func<IConnectionFactory> connectionFactoryBuilder, IEnumerable<Type> dapperQueries, IEnumerable<Type> dapperQueriesThatHaveNoParameters, IEnumerable<Type> dapperQueryParameters)
         {
-            _connectionFactory = connectionFactory;
+            _connectionFactoryBuilder = connectionFactoryBuilder;
             _dapperQueries = dapperQueries;
             _dapperQueriesThatHaveNoParameters = dapperQueriesThatHaveNoParameters;
             _dapperQueryParameters = dapperQueryParameters;
@@ -50,10 +50,10 @@ namespace Tailor.Test
                 Task.FromResult(_dapperQueryParameters.MustConformTo(Convention.MustHaveAppropriateConstructors)),
                 Task.FromResult(
                     _dapperQueries.Union(_dapperQueriesThatHaveNoParameters)
-                    .MustConformTo(new DapperQueriesMustNotDoSelectStar(_connectionFactory))),
-                Task.FromResult(_dapperQueries.MustConformTo(new DapperQueriesWithParametersMustHaveAParametersListThatMatchesTheDefinedSql(_connectionFactory))),
-                _dapperQueries.MustConformTo(new DapperQueriesWithParametersMustExecuteSuccessfully(_connectionFactory, exceptionalExceptions)),
-                _dapperQueriesThatHaveNoParameters.MustConformTo(new DapperQueriesWithoutParametersMustExecuteSuccessfully(_connectionFactory)),
+                    .MustConformTo(new DapperQueriesMustNotDoSelectStar(_connectionFactoryBuilder()))),
+                Task.FromResult(_dapperQueries.MustConformTo(new DapperQueriesWithParametersMustHaveAParametersListThatMatchesTheDefinedSql(_connectionFactoryBuilder()))),
+                _dapperQueries.MustConformTo(new DapperQueriesWithParametersMustExecuteSuccessfully(_connectionFactoryBuilder(), exceptionalExceptions)),
+                _dapperQueriesThatHaveNoParameters.MustConformTo(new DapperQueriesWithoutParametersMustExecuteSuccessfully(_connectionFactoryBuilder())),
             };
         }
     }
